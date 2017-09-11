5ea81e01eac7cc38c6d6f7566d0a0bbfe23f0ee7
bmcmenamin <bmcmenamin@gmail.com>
2017-09-10 21:08:10 -0400

    documenting broken stick

--- fa_kit/broken_stick.py
+++ fa_kit/broken_stick.py
@@ -1,159 +1,159 @@
-"""
-The broken-stick object used for calculating the
-number of components to retain
+""" This module defines the BrokenStick class
 """
 
 import numpy as np
 
+
+def weighted_moments(values, weights):
+    """Return weighted mean and weighted standard deviation of a sequence"""
+
+    w_mean = np.average(values, weights=weights)
+
+    sq_err = (values - w_mean)**2
+    w_var = np.average(sq_err, weights=weights)
+
+    w_std = np.sqrt(w_var)
+
+    return w_mean, w_std
+
+
+def calc_broken_stick(dim):
+    """Generate values for broken-stick distribution of certain length"""
+
+    if dim < 1:
+        raise ValueError('Boken stick dimension must be greater than 1')
+
+    values = np.array([
+        1.0 / (i + 1)
+        for i in range(dim)
+        ])
+
+    values = np.sum(values) - np.insert(np.cumsum(values)[:-1], 0, 0)
+    values /= dim
+
+    return values
+
+
+def fisher_info(data):
+    """Return Fisher information of a sequence"""
+
+    if data.min() < 0:
+        raise ValueError(
+            'Fiser info only works on non-negative '
+            'data. Current minimum: {}'.format(data.min())
+            )
+
+    # padding and renormalizing to avoid zero-values in log
+    pad = 1.0e-16 * np.mean(data)
+    data_pad = data - data.min() + pad
+    data_pad /= np.sum(data_pad)
+
+    data_log = np.log(data_pad)
+
+    data_fi = np.abs(
+        np.gradient(np.gradient(data_log))
+        )
+
+    return data_fi
+
+
+def fit_to_data(distro_values, target_data, weights=None, fit_on_log=True):
+    """
+    Scale and shift the values in `distro_values` to align with a
+    set of observed data in the sequence `target_data`
+
+    If `fit_on_log` is True, the values in the BrokenStick object and
+    `target_data` will be log-transformed before fitting. This is useful
+    when aligning series that are strictly non-negative (e.g., eigenvalues
+    from P.S.D. matrices).
+
+    The array `weights` will determine how much importance is placed on
+    matching each particular point in the series.
+    """
+
+    if fit_on_log:
+        targ_trans = np.log(target_data + 1)
+        dist_trans = np.log(distro_values + 1)
+    else:
+        targ_trans = target_data
+        dist_trans = distro_values
+
+    targ_wmean, targ_wsd = weighted_moments(targ_trans, weights)
+    dist_wmean, dist_wsd = weighted_moments(dist_trans, weights)
+
+    scale = targ_wsd / dist_wsd
+    shift = targ_wmean - scale*dist_wmean
+
+    dist_trans_fit = scale*dist_trans + shift
+
+    dist_fit = np.exp(dist_trans_fit) - 1
+
+    return dist_fit
+
+
 class BrokenStick(object):
     """
-    Broken-stick distributed values that can be scaled/shifted
-    to align with other distributions
+    BrokenStick object has a series of values in the attribute `values` that are
+    generated to conform to a broken stick distribution. These values can be
+    shifted and scaled to align with a target distribution that you suspect is
+    distributed in a broken-stick-esque manner.
     """
 
     def __init__(self, in_vals, **kwargs):
 
         if isinstance(in_vals, (float, int)):
-            self.values = self._calc_broken_stick(in_vals)
+            self.values = calc_broken_stick(in_vals)
         else:
-            self.values = self._calc_broken_stick(len(in_vals))
+            self.values = calc_broken_stick(len(in_vals))
 
             fit_on_log = kwargs.get('fit_on_log', True)
             self.rescale_broken_stick(in_vals, fit_on_log=fit_on_log)
 
-    @staticmethod
-    def _calc_broken_stick(dim):
-        """
-        generate the values for a length "dim" broken
-        stick distribution
-        """
-
-        if dim < 1:
-            raise ValueError('Boken stick dimension must be greater than 1')
-
-        values = np.array([
-            1.0 / (i + 1)
-            for i in range(dim)
-            ])
-
-        values = np.sum(values) - np.insert(np.cumsum(values)[:-1], 0, 0)
-        values /= dim
-
-        return values
-
-    @staticmethod
-    def _weighted_moments(values, weights):
-        """
-        returns the mean and standarad deviation of values
-        with weights placed on each of the values accoridng to weights
-        """
-
-        w_mean = np.average(values, weights=weights)
-
-        sq_err = (values - w_mean)**2
-        w_var = np.average(sq_err, weights=weights)
-
-        w_std = np.sqrt(w_var)
-
-        return w_mean, w_std
-
-    @classmethod
-    def _fit_to_data(cls, distro_values, target_data,
-        weights=None, fit_on_log=True):
-        """
-        scale and shift the values in the broken stick to
-        best match a set of observed data in target_data
-
-        to account for the purely positive distribution of broken stick values,
-        matching is done on log-transformed values and then backprojected to
-        the original.
-
-        weights is an array that determines how much weight is placed on
-        fitting to each of the values in data.
-        """
-
-
-        if fit_on_log:
-            targ_trans = np.log(target_data + 1)
-            dist_trans = np.log(distro_values + 1)
-        else:
-            targ_trans = target_data
-            dist_trans = distro_values
-
-        targ_wmean, targ_wsd = cls._weighted_moments(targ_trans, weights)
-        dist_wmean, dist_wsd = cls._weighted_moments(dist_trans, weights)
-
-        scale = targ_wsd / dist_wsd
-        shift = targ_wmean - scale*dist_wmean
-
-        dist_trans_fit = scale*dist_trans + shift
-
-        dist_fit = np.exp(dist_trans_fit) - 1
-
-        return dist_fit
-
-    @staticmethod
-    def _is_sorted(values, ascending=True):
-
-        for i, j in zip(values, values[1:]):
-
-            if ascending and i > j:
-                return False
-
-            if not ascending and i < j:
-                return False
-
-        return True
-
-    @staticmethod
-    def _fisher_info(data):
-
-        pad = 1.0e-16 * np.mean(data)
-        data_pad = data - data.min() + pad
-        data_pad /= np.sum(data_pad)
-
-        log_data = np.log(data_pad)
-
-        f_info = np.abs(
-            np.gradient(np.gradient(log_data))
-            )
-        return f_info
 
     def rescale_broken_stick(self, target_data, fit_on_log=True):
         """
-        rescale the broken stick distro's values to align with
-        provided target_data. alignment happns by linear shift/scale
-        on log-transformed values.
-
+        Align values in the BrokenStick object with the provided target_data.
+        Changes are made in-place
         """
 
+        # Determine how to (un)sort the target values based on abs-mag
+        targ_sort_idx = np.argsort(-np.abs(target_data))
+        targ_unsort_idx = np.argsort(targ_sort_idx)
 
-        targ_is_sorted = self._is_sorted(target_data, ascending=False)
-        if not targ_is_sorted:
-            raise ValueError('Target data is not sorted')
+        # Determine how to (un)sort the BrokenStick values based on abs-mag
+        self_sort_idx = np.argsort(-np.abs(self.values))
+        self_unsort_idx = np.argsort(self_sort_idx)
 
-        sort_idx = np.argsort(-np.abs(target_data))
-        unsort_idx = np.argsort(sort_idx)
+        """
+        Calculate the weights for alignment using the cumulative inverse
+        Fisher information of the target values. This will make sure to
+        put the most emphasis on fitting 'flat' sections and ignore the
+        early parts of the distribution where the broken stick is likely to
+        diverge from the observed values
+        """
 
-        inv_fisher_info = self._fisher_info(np.abs(target_data[sort_idx])) ** -2.0
+        inv_fisher_info = fisher_info(
+            np.abs(target_data[targ_sort_idx])
+            ) ** -2.0
+
         weights = np.cumsum(inv_fisher_info)
         weights -= weights.min()
 
-        bs_sorted_fit = self._fit_to_data(
-            self.values,
-            np.abs(target_data[sort_idx]),
+        fit_values_sorted = fit_to_data(
+            np.abs(self.values[self_sort_idx]),
+            np.abs(target_data[targ_sort_idx]),
             weights,
             fit_on_log=fit_on_log
             )
 
-        bs_unsorted_fit = bs_sorted_fit[unsort_idx] * np.sign(target_data)
-        self.values = bs_unsorted_fit
+        fit_values = fit_values_sorted[self_unsort_idx] * np.sign(target_data)
+        self.values = fit_values
 
 
     def find_where_target_exceeds(self, target_data):
         """
-        Return the indices where the absolute value of the
-        target vector exceed the broken stick distro
+        Return the indices where the absolute value of target_data exceed
+        the absolute value of the BrokenStick values
         """
 
         good_idx_pos = []
--- tests/test_BrokenStick.py
+++ tests/test_BrokenStick.py
@@ -8,80 +8,78 @@ import numpy as np
 from fa_kit.broken_stick import BrokenStick
 
 
+def is_sorted(values, ascending=True):
+    """Return True if a sequence is sorted"""
+
+    for i, j in zip(values, values[1:]):
+
+        if ascending and i > j:
+            return False
+
+        if not ascending and i < j:
+            return False
+
+    return True
+
+
 #
 # Testing input validation
 #
 
 TEST_DIM = 10
 
-
-def test_is_sorted():
-
-    list_ascend = list(range(10))
-    list_descend = list(range(10))[::-1]
-
-    assert BrokenStick._is_sorted(list_ascend, ascending=True)
-    assert BrokenStick._is_sorted(list_descend, ascending=False)
-    assert not BrokenStick._is_sorted(list_ascend, ascending=False)
-    assert not BrokenStick._is_sorted(list_descend, ascending=True)
-
-
-def _valid_bs_distro(vals):
-
+def _test_valid_bs_distro(vals):
     assert len(vals) == TEST_DIM
-    assert BrokenStick._is_sorted(vals, ascending=False)
-
+    assert is_sorted(vals, ascending=False)
 
 def test_init():
-
-    bs = BrokenStick(TEST_DIM)
-    _valid_bs_distro(bs.values)
-    assert sum(bs.values) == 1
-
+    """ Test the BrokenStick initialized correctly"""
+    broken_stick = BrokenStick(TEST_DIM)
+    _test_valid_bs_distro(broken_stick.values)
+    assert sum(broken_stick.values) == 1
 
 def test_init_neg():
-
+    """ Test the BrokenStick throws an error with bad init inputs"""
     with pytest.raises(ValueError):
-        bs = BrokenStick(-TEST_DIM)
+        BrokenStick(-TEST_DIM)
 
 
 #
 # Testing fit to distro
 #
 
-
 def _fit_to_self(scale, shift):
 
     orig_vals = BrokenStick(TEST_DIM).values
     targ_data = (scale * orig_vals) + shift
 
-    _valid_bs_distro(orig_vals)
-    _valid_bs_distro(targ_data)
+    _test_valid_bs_distro(orig_vals)
+    _test_valid_bs_distro(targ_data)
 
-    bs = BrokenStick(targ_data)
-    _valid_bs_distro(bs.values)
+    broken_stick = BrokenStick(targ_data)
+    _test_valid_bs_distro(broken_stick.values)
 
     if shift > 0.0:
-        assert np.mean(orig_vals) < np.mean(bs.values)
+        assert np.mean(orig_vals) < np.mean(broken_stick.values)
 
     if shift < 0.0:
-        assert np.mean(orig_vals) > np.mean(bs.values)
+        assert np.mean(orig_vals) > np.mean(broken_stick.values)
 
     if scale > 1.0:
-        assert np.std(orig_vals) < np.std(bs.values)
+        assert np.std(orig_vals) < np.std(broken_stick.values)
 
     if scale < 1.0 and scale > 0.0:
-        assert np.std(orig_vals) > np.std(bs.values)
+        assert np.std(orig_vals) > np.std(broken_stick.values)
 
 
 def test_scale_and_fit():
-
+    """Test that BrokenStick can align to scaled-up/down copies"""
     for scale in [0.5, 1.0, 10.0]:
         _fit_to_self(scale, 0.0)
 
 
 def test_shift_and_fit():
-
+    """Test that BrokenStick can align to shifted-up/down copies"""
     for shift in [-0.01, 0.0, 0.01]:
         _fit_to_self(1.0, shift)
 
@@ -90,21 +88,21 @@ def test_shift_and_fit():
 # Find extremes
 #
 
-
 @pytest.fixture
 def shifted_bs():
+    """Make a BrokenStick thats half positive, half negative"""
     bs_orig = BrokenStick(TEST_DIM)
     orig_vals = bs_orig.values
     shift_values = np.concatenate([orig_vals[0::2], -orig_vals[1::2]])
     shift_values = np.array(sorted(shift_values)[::-1])
 
-    bs = BrokenStick(shift_values)
-    _valid_bs_distro(bs.values)
+    broken_stick = BrokenStick(shift_values)
 
-    return bs
+    return broken_stick
 
 
 def test_find_positive_values(shifted_bs):
+    """Test that we can find extreme positive values"""
 
     idx_to_find_pos = list(range(2))
     idx_to_find = sorted(idx_to_find_pos)
@@ -116,7 +114,9 @@ def test_find_positive_values(shifted_bs):
 
     assert idx_found == idx_to_find
 
+
 def test_find_negative_values(shifted_bs):
+    """Test that we can find extreme negative values"""
 
     idx_to_find_neg = list(range(TEST_DIM-1, TEST_DIM-3, -1))
     idx_to_find = sorted(idx_to_find_neg)
@@ -128,7 +128,9 @@ def test_find_negative_values(shifted_bs):
 
     assert idx_found == idx_to_find
 
+
 def test_find_positve_and_negative_values(shifted_bs):
+    """Test that we can find extreme postive and negative values"""
 
     idx_to_find_pos = list(range(2))
     idx_to_find_neg = list(range(TEST_DIM-1, TEST_DIM-3, -1))
